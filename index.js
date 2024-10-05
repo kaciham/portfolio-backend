9d1307d5e361efab47034d1cb364ffe05693aac3
kaciham <118285179+kaciham@users.noreply.github.com>
2024-10-05 16:51:01 +0200

    env implementation

--- index.js
+++ index.js
@@ -1,27 +1,35 @@
-const express = require("express");
-const app = express()
-const multer = require("multer")
-const port = 3002;
-const morgan = require("morgan");
-const cors = require("cors")
-const path = require("path")
 
 require("./configs/db.config")
 require('dotenv').config();
 
+const express = require("express");
+const app = express()
+const multer = require("multer")
+const port = process.env.PORT
+const morgan = require("morgan");
+const cors = require("cors")
+const path = require("path")
+
+console.log('====================================');
+console.log(port);
+console.log('====================================');
+
+
 const router = require("./routes/routesIndex.js");
+const { config } = require("dotenv");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
 app.use(morgan("tiny"))
 app.use("/api", router);
+
 ///Images static directory
 app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use('/imagesProject', express.static(path.join(__dirname, 'imagesProject')));
 app.use('/imagesPortfolio', express.static(path.join(__dirname, 'imagesPortfolio')));
-app.use('/pdf', express.static(path.join(__dirname, 'pdf')));
+app.use('/pdf', express.static(path.join(__dirname,  'pdf')));
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`)
-})
+})  
