22178adb5851da2fbb40c2c53d4b948d78400d6b
kaciham <118285179+kaciham@users.noreply.github.com>
2024-11-10 19:53:31 +0100

    add fanwebapp

--- middlewares/multer-config-imageUrl.js
+++ middlewares/multer-config-imageUrl.js
@@ -6,7 +6,8 @@ const fs = require('fs');
 const MIME_TYPES = {
     'image/jpg': 'jpg',
     'image/jpeg': 'jpg',
-    'image/png': 'png'
+    'image/png': 'png',
+    'image/webp': 'webp',
 };
 
 const storage = multer.diskStorage({
