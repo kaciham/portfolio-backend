641e6f939fb21f139760e7d062c454cc776b306d
kaciham <118285179+kaciham@users.noreply.github.com>
2025-04-26 19:23:53 +0200

    cors issue

--- index.js
+++ index.js
@@ -15,11 +15,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 const corsOptions = {
-    "Access-Control-Allow-Origin": "https://kacihamroun.website",
+    "Access-Control-Allow-Origin": ['*'],
     "Access-Control-Allow-Methods": "GET,POST,PUT,DELETE,OPTIONS",
     "Access-Control-Allow-Headers": "Content-Type,Authorization",
     "Access-Control-Allow-Credentials": true,
-    "Access-Control-Allow-Max-Age": 86400
+    "Access-Control-Allow-Max-Age": 8640
 };
 
 app.use(cors(corsOptions));
